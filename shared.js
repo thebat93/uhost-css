17ddbb5b1e4f725ee240a068cfd98be417d09261
Igor <ais_93@mail.ru>
2018-06-02 19:42:17 +0300

    134 Adding a Side Navigation Bar

--- shared.js
+++ shared.js
@@ -1,6 +1,9 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan .button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
@@ -8,3 +11,20 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
         modal.style.display = 'block';
     });
 }
+
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+toggleButton.addEventListener('click', function() {
+    backdrop.style.display = 'block';
+    mobileNav.style.display = 'block';
+});
