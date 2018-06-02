3c6de7390c0657136f1365d9919440c788449d2a
Igor <ais_93@mail.ru>
2018-06-02 20:02:53 +0300

    138 Cleaning Up our Code

--- shared.js
+++ shared.js
@@ -17,10 +17,14 @@ backdrop.addEventListener('click', function() {
     closeModal();
 });
 
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-    modal.classList.remove('open');
+    if (modal) {
+        modal.classList.remove('open');
+    }
     backdrop.classList.remove('open');
 }
 
