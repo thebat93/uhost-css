b0c19a031b6acddffdee91de2699e4b1fff40d13
Igor <ais_93@mail.ru>
2018-06-02 19:50:30 +0300

    136 Manipulating Element Classes

--- shared.js
+++ shared.js
@@ -7,24 +7,24 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
 backdrop.addEventListener('click', function() {
-    mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
 modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function() {
-    backdrop.style.display = 'block';
-    mobileNav.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
